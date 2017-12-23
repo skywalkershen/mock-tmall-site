603a127ecc3babb88bfd337e4103e02bbd27b10a
skywalkershen <skywalkershen@hotmail.com>
2017-12-23 13:10:43 -0800

    add catear effect

---
+++ js/catear.js
@@ -0,0 +1,15 @@
+$(function(){
+    $("div.rightMenu span").mouseleave(function(){
+        $("img.catear").hide();
+    })
+    $("div.rightMenu span").mouseenter(function(){
+        var left=$(this).position().left;
+        var top=$(this).position().top;
+        var width = $(this).css("width");
+        var catearLeft = parseInt(left) + parseInt(width) / 2;
+        $("img.catear").css("left", catearLeft);
+        $("img.catear").css("top", top + 26);
+        $("img.catear").fadeIn(500);
+        console.log("catear at left: " + catearLeft +" top: " + top);
+    })
+});
